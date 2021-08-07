6f6bdaf5167b6e8366578e2f7ca6b1f871aa7322
Dong Yeop Lee <dongy7@gmail.com>
2021-08-07 11:53:33 -0500

    load notes from api

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchNotes } from './notesSlice'
 
 export const NotesList = () => {
-  const notes = useSelector((state) => state.notes)
+  const dispatch = useDispatch()
+  const notes = useSelector((state) => state.notes.notes)
 
-  const renderedNotes = notes.map((note) => (
-    <tr key={note.id}>
-      <td>{note.title}</td>
-    </tr>
-  ))
+  const notesStatus = useSelector((state) => state.notes.status)
+  const error = useSelector((state) => state.notes.error)
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>{renderedNotes}</tbody>
-    </table>
-  )
+  useEffect(() => {
+    if (notesStatus === 'idle') {
+      dispatch(fetchNotes())
+    }
+  }, [notesStatus, dispatch])
+
+  let content
+
+  if (notesStatus === 'loading') {
+    content = <div>Loading...</div>
+  } else if (notesStatus === 'succeeded') {
+    const renderedNotes = notes.map((note) => (
+      <tr key={note.id}>
+        <td>{note.title}</td>
+      </tr>
+    ))
+
+    content = (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>{renderedNotes}</tbody>
+      </table>
+    )
+  } else if (notesStatus === 'failed') {
+    return <div>{error}</div>
+  }
+
+  return <div>{content}</div>
 }
--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -1,14 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = [
-  { id: '1', title: 'My note 1' },
-  { id: '2', title: 'My note 2' },
-]
+const initialState = {
+  notes: [],
+  status: 'idle',
+  error: '',
+}
+
+export const fetchNotes = createAsyncThunk('notes/fetchNotes', async () => {
+  const response = await axios.get('/api/notes')
+  return response.data.notes
+})
 
 export const notesSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchNotes.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [fetchNotes.fulfilled]: (state, action) => {
+      state.status = 'succeeded'
+      state.notes = state.notes.concat(action.payload)
+    },
+    [fetchNotes.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    },
+  },
 })
 
 export default notesSlice.reducer
